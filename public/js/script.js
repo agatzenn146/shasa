da3f796060481f7f23934524ebf5c7eae535dbee
agatzenn146 <156975813+agatzenn146@users.noreply.github.com>
2026-02-19 10:15:55 +1000

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -74,16 +74,4 @@ $(document).ready(function() {
     
   });
 
-  const slides = document.querySelectorAll('.slide');
-  let index = 0;
-
-  function showSlide() {
-    slides.forEach(slide => slide.classList.remove('active'));
-    index++;
-    if (index >= slides.length) {
-      index = 0;
-    }
-    slides[index].classList.add('active');
-  }
-
-  setInterval(showSlide, 4000); // Change every 4 seconds
+ 

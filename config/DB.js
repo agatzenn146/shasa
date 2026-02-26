c61c00d893034b2db058fa0e17294a7007445f96
Agatha Nop <anop@PNGPOWER.local>
2026-02-27 08:18:28 +1000

    Switch to Neon PostgreSQL

--- config/DB.js
+++ config/DB.js
@@ -1,18 +1,11 @@
-const mysql = require('mysql2');
+const { Pool } = require('pg');
+require('dotenv').config();
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', // XAMPP default
-  database: 'shasa_db'
-});
-
-db.connect(err => {
-  if (err) {
-    console.error('MySQL connection failed:', err);
-  } else {
-    console.log('MySQL connected');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
-module.exports = db;
+module.exports = pool;
--- models/DB.js
+++ models/DB.js
@@ -1,19 +1,11 @@
-const mysql = require('mysql2');
-require('dotenv').config(); // make sure this is at the top
+const { Pool } = require('pg');
+require('dotenv').config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',  // XAMPP default
-  database: process.env.DB_NAME || 'shasa_db'
-});
-
-db.connect(err => {
-  if (err) {
-    console.error('MySQL connection failed:', err);
-  } else {
-    console.log('MySQL connected');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
-module.exports = db;
+module.exports = pool;

7f8f1d021a29d32393a735276adbe92219bc4be2
agatzenn146 <156975813+agatzenn146@users.noreply.github.com>
2026-02-27 09:42:09 +1000

    Refactor database connection configuration

--- models/DB.js
+++ models/DB.js
@@ -1,11 +1,8 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
+  ssl: { rejectUnauthorized: false }
 });
-
-module.exports = pool;
